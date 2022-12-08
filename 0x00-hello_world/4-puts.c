c4507a1d88e2f4181918b485d3861a3fc32c0a51
Jesse <esaujesse@yahoo.com>
2022-12-08 03:07:13 -0800

    Mandatory task 0 to 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ *
+ * Description: prints "\"Programming is like building a multilingual puzzle
+ * Return : 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
